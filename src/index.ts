aa05fc557321bf7100e26b8b5c6571032a60be2b
Julli4n <11952329+Julli4n@users.noreply.github.com>
2025-09-18 18:37:08 -0700

    fix

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import HTTPClient from "./classes/HTTPClient";
+import { HTTPResponse } from "./classes/HTTPResponse";
+import { RESTError } from "./classes/RESTError";
+
+export { HTTPClient, HTTPResponse, RESTError };
